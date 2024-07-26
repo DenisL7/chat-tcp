0b4525bea1c49447ba89ac5166de374f41ad738b
Denis <denis_lupach@ukr.net>
2024-07-26 11:24:47 +0300

    server changes

---
+++ chat-tcp-fx/src/main/java/EventHandler.java
@@ -0,0 +1,10 @@
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+
+public class EventHandler implements javafx.event.EventHandler<ActionEvent> {
+
+    @Override
+    public void handle(ActionEvent actionEvent) {
+
+    }
+}
